8b1c4fa8ace84ba2fadded22b703fac2ab07586c
Gabriel-Simon07 <gabrielsimon775@gmail.com>
2021-03-25 22:25:05 -0300

    Conexão do C# com o banco de dados

---
+++ Conexão/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Conexão
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblRegistros = new System.Windows.Forms.DataGridView();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnVisualizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnModifica = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tblRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblRegistros
+            // 
+            this.tblRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblRegistros.Location = new System.Drawing.Point(112, 21);
+            this.tblRegistros.Name = "tblRegistros";
+            this.tblRegistros.Size = new System.Drawing.Size(559, 175);
+            this.tblRegistros.TabIndex = 0;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(632, 380);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 1;
+            this.btnGravar.Text = "GRAVAR";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(125, 281);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(364, 20);
+            this.txtTitulo.TabIndex = 2;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(125, 342);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(364, 20);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // btnVisualizar
+            // 
+            this.btnVisualizar.Location = new System.Drawing.Point(619, 258);
+            this.btnVisualizar.Name = "btnVisualizar";
+            this.btnVisualizar.Size = new System.Drawing.Size(88, 23);
+            this.btnVisualizar.TabIndex = 4;
+            this.btnVisualizar.Text = "VISUALIZAR";
+            this.btnVisualizar.UseVisualStyleBackColor = true;
+            this.btnVisualizar.Click += new System.EventHandler(this.btnVisualizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "TÍTULO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "INSERIR DADOS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DESCRIÇÂO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(125, 403);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(46, 20);
+            this.txtId.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Excluir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnModifica
+            // 
+            this.btnModifica.Location = new System.Drawing.Point(250, 403);
+            this.btnModifica.Name = "btnModifica";
+            this.btnModifica.Size = new System.Drawing.Size(63, 23);
+            this.btnModifica.TabIndex = 11;
+            this.btnModifica.Text = "Alterar";
+            this.btnModifica.UseVisualStyleBackColor = true;
+            this.btnModifica.Click += new System.EventHandler(this.btnModifica_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModifica);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVisualizar);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.tblRegistros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblRegistros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tblRegistros;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnVisualizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnModifica;
+    }
+}
+
---
+++ Conexão/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Conexão
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection mConn;
+        private MySqlDataAdapter mAdapter;
+        private DataSet mDataSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mostraResultados();
+        }
+
+        private void mostraResultados()
+
+        {
+
+            mDataSet = new DataSet();
+
+            mConn = new MySqlConnection("server=localhost;user id=root;sslmode=None;database=testeBd");
+
+            mConn.Open();
+
+            //cria um adapter utilizando a instrução SQL para aceder à tabela
+
+            mAdapter = new MySqlDataAdapter("SELECT * FROM LIVROS ORDER BY id", mConn);
+
+            //preenche o dataset através do adapter
+
+            mAdapter.Fill(mDataSet, "livros");
+
+            //atribui o resultado à propriedade DataSource da dataGridView
+
+            tblRegistros.DataSource = mDataSet;
+
+            tblRegistros.DataMember = "livros";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVisualizar_Click(object sender, EventArgs e)
+        {
+            mostraResultados();
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            // Início da Conexão com indicação de qual o servidor, nome de base de dados e utilizar
+
+            mConn = new MySqlConnection("server=localhost;user id=root;sslmode=None;database=testeBd");
+
+            // Abre a conexão
+
+            mConn.Open();
+
+            //Query SQL
+
+            MySqlCommand command = new MySqlCommand("INSERT INTO LIVROS(titulo, descricao)" +"VALUES('" + txtTitulo.Text + "','" + txtDescricao.Text + "')", mConn);
+
+            //Executa a Query SQL
+
+            command.ExecuteNonQuery();
+
+            // Fecha a conexão
+
+            mConn.Close();
+
+            //Mensagem de Sucesso
+
+            MessageBox.Show("Gravado com Sucesso!", "Informação", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+
+            // Método criado para que quando o registo é gravado, automaticamente a dataGridView efectue um "Refresh"
+
+            mostraResultados();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Início da Conexão com indicação de qual o servidor, nome de base de dados e utilizar
+
+            mConn = new MySqlConnection("server=localhost;user id=root;sslmode=None;database=testeBd");
+
+            // Abre a conexão
+
+            mConn.Open();
+
+            //Query SQL
+
+            MySqlCommand command = new MySqlCommand("DELETE FROM LIVROS WHERE ID =('"  + txtId.Text + "')", mConn);
+
+            //Executa a Query SQL
+
+            command.ExecuteNonQuery();
+
+            // Fecha a conexão
+
+            mConn.Close();
+
+            //Mensagem de Sucesso
+
+            MessageBox.Show("Alterado com Sucesso!", "Informação", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+
+            // Método criado para que quando o registo é gravado, automaticamente a dataGridView efectue um "Refresh"
+
+            mostraResultados();
+        }
+
+        private void btnModifica_Click(object sender, EventArgs e)
+        {
+            // Início da Conexão com indicação de qual o servidor, nome de base de dados e utilizar
+
+            mConn = new MySqlConnection("server=localhost;user id=root;sslmode=None;database=testeBd");
+
+            // Abre a conexão
+
+            mConn.Open();
+
+            //Query SQL
+
+            MySqlCommand command = new MySqlCommand("UPDATE LIVROS SET titulo = '" + txtTitulo.Text + "',descricao='" + txtDescricao.Text + "' WHERE ID=" + txtId.Text , mConn);
+            
+
+            //Executa a Query SQL
+
+            command.ExecuteNonQuery();
+
+            // Fecha a conexão
+
+            mConn.Close();
+
+            //Mensagem de Sucesso
+
+            MessageBox.Show("Alterado com Sucesso!", "Informação", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+
+            // Método criado para que quando o registo é gravado, automaticamente a dataGridView efectue um "Refresh"
+
+            mostraResultados();
+        }
+    }
+    }
+
